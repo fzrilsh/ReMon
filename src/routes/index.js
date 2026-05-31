afab60911668f2eb62a2791644528ca6a7981171
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2026-05-31 11:57:44 +0700

    feat: implement user settings page for bank account management and integrate into split bill view

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,14 +9,17 @@ const debtRoutes = require('./debts');
 const pwaRoutes = require('./pwa');
 const publicRoutes = require('./public');
 const notificationRoutes = require('./notifications');
+const settingsRoutes = require('./settings');
 
 router.use('/auth', authRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/transactions', transactionRoutes);
 router.use('/split-bills', splitBillRoutes);
 router.use('/debts', debtRoutes);
+router.use('/settings', settingsRoutes);
 router.use('/', pwaRoutes);
 router.use('/split', publicRoutes);
 router.use('/notifications', notificationRoutes);
+
 
 module.exports = router;
